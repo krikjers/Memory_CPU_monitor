a400e7dab91280d862dea6756348fa26ede46624
Unknown <krikjers@stud.ntnu.no>
2017-04-10 22:49:50 +0200

    output CPU usage

--- Memory_CPU_monitor/Memory_CPU_monitor/Program.cs
+++ Memory_CPU_monitor/Memory_CPU_monitor/Program.cs
@@ -4,12 +4,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.Diagnostics;       //contains performance counter  
+using System.Threading;
+
 namespace Memory_CPU_monitor
 {
     class Program
     {
+
         static void Main(string[] args)
         {
+            //get CPU load in percentage
+            PerformanceCounter perfCPUCount = new PerformanceCounter("Processor Information", "% Processor Time", "_Total");
+
+            while(true)
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("CPU load: {0}", perfCPUCount.NextValue());       //perfCPUCount.NextValue() - return current number
+            }
         }
     }
 }
